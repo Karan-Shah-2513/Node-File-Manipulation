e44c49d9d5c38bee409fef2aef375ad27644ea31
Karan Shah <karandood28@gmail.com>
2022-10-19 17:49:53 +0530

    Learnt how to use fsExtra to move files and folders

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const chokidar = require("chokidar");
+const fs = require("fs");
+const fsExtra = require("fs-extra");
 
 const pathToWatch = "./watch-here";
-
 const watcher = chokidar.watch(pathToWatch);
 
 watcher
-  .on("add", (path) => console.log(`File ${path} has been added`))
+  .on("add", (path) => {
+    console.log(`File ${path} has been added`);
+    // console.log("Now moving the file to universe");
+    // // fs.copyFile();
+
+    // const fileExtension = path.split(".").pop();
+    // const fileName = path.split("\\").pop();
+    // fsExtra.move(path, `./universe/${fileName}`, (err) => {
+    //   console.error(err);
+    // });
+  })
   .on("change", (path) => console.log(`File ${path} has been changed`))
   .on("unlink", (path) => console.log(`File ${path} has been removed`));
 
 // More possible events.
 watcher
-  .on("addDir", (path) => console.log(`Directory ${path} has been added`))
+  .on("addDir", (path) => {
+    console.log(`Directory ${path} has been added`);
+    setTimeout(() => {
+      if (path != "watch-here") {
+        const name = path.split("\\").pop();
+        console.log(path);
+        console.log(path + "/" + name);
+        fsExtra.move(path, `./universe/${name}`, (err) => {
+          console.error(err);
+        });
+      }
+    }, 10000);
+  })
   .on("unlinkDir", (path) => console.log(`Directory ${path} has been removed`))
   .on("error", (error) => console.log(`Watcher error: ${error}`))
   .on("ready", () => console.log("Initial scan complete. Ready for changes"))
