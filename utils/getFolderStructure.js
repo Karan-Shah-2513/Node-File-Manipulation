f1da83c359c39ba1bcbe7f1acf731c9e03d9ca57
Karan Shah <karandood28@gmail.com>
2023-01-02 23:14:01 +0530

    Improved Functionality of getFolderStructure utility

--- utils/getFolderStructure.js
+++ utils/getFolderStructure.js
@@ -1,15 +1,25 @@
 //TODO: Add function for getting the full file structure
 import filesystem from "fs";
+/*
+Returns an array of objects containing paths of all the folders and files in the main directory
+*/
 const getFolderStructure = (dir) => {
-  var results = [];
+  let results = [];
 
   filesystem.readdirSync(dir).forEach(function (file) {
+    let obj = {};
     file = dir + "/" + file;
-    var stat = filesystem.statSync(file);
+    obj.path = file;
+    const stat = filesystem.statSync(file);
 
     if (stat && stat.isDirectory()) {
+      results.push(obj);
+      obj.isDirectory = true;
       results = results.concat(getFolderStructure(file));
-    } else results.push(file);
+    } else {
+      obj.isDirectory = false;
+      results.push(obj);
+    }
   });
 
   return results;
