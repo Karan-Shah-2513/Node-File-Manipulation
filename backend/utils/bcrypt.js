ef54d1b49a9144bcc2711405d87023bdac562332
Karan Shah <karandood28@gmail.com>
2023-01-10 16:29:00 +0530

    Moved all server files to backend and initialized frontend with create next app

---
+++ backend/utils/bcrypt.js
@@ -0,0 +1,36 @@
+// Requiring module
+import bcrypt from "bcryptjs";
+
+const checkPassword = (input, password) => {
+  bcrypt.genSalt(10, function (err, Salt) {
+    // The bcrypt is used for encrypting password.
+    bcrypt.hash(password, Salt, function (err, hash) {
+      if (err) {
+        return console.log("Cannot encrypt");
+      }
+
+      hashedPassword = hash;
+      console.log(hash);
+
+      bcrypt.compare(password, hashedPassword, async function (err, isMatch) {
+        // Comparing the original password to
+        // encrypted password
+        if (isMatch) {
+          console.log("Encrypted password is: ", password);
+          console.log("Decrypted password is: ", hashedPassword);
+        }
+
+        if (!isMatch) {
+          // If password doesn't match the following
+          // message will be sent
+          console.log(hashedPassword + " is not encryption of " + password);
+        }
+      });
+    });
+  });
+};
+
+let password = "pass123";
+var hashedPassword;
+
+// Encryption of the string password
